172c6f24280c192e62b99b3be954b5e400a4e706
huhuhang <huhuhang@gmail.com>
2019-02-28 16:57:40 +0800

    v1

---
+++ run.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import random
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    cat_api = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"
+    dog_api = "https://dog.ceo/api/breeds/image/random"
+    cat_url = requests.get(cat_api).json()[0]['url']
+    dog_url = requests.get(dog_api).json()['message']
+
+    dog_json = jsonify({"species": "dog", "url": dog_url})
+    cat_json = jsonify({"species": "cat", "url": cat_url})
+
+    random_url = random.choice([dog_json, cat_json])
+
+    return random_url
+
+@app.route('/cat')
+def cat():
+    cat_api = "https://api.thecatapi.com/v1/images/search?mime_types=jpg"
+    cat_url = requests.get(cat_api).json()[0]['url']
+    return jsonify({"url": cat_url})
+
+@app.route('/dog')
+def dog():
+    dog_api = "https://dog.ceo/api/breeds/image/random"
+    dog_url = requests.get(dog_api).json()['message']
+    return jsonify({"url": dog_url})
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=True)
